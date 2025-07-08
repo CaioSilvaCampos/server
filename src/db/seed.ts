9c63f29dd39f945532053da5e52c21533e099292
CaioSilvaCampos <caioiosilva19@gmail.com>
2025-07-07 23:03:15 -0300

    add get rooms

---
+++ src/db/seed.ts
@@ -0,0 +1,28 @@
+// biome-ignore assist/source/organizeImports: <so funciona assim sla pq kk>
+import 'dotenv/config'; 
+import { reset, seed} from 'drizzle-seed'
+import { db,sql } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+import { env } from '../env.ts';
+
+await reset(db, {schema})
+
+await seed(db, schema).refine(f =>{
+    return {
+        rooms:{
+            count: 20,
+            columns:{
+                name: f.companyName(),
+                description: f.loremIpsum()
+            },
+        },
+    }
+})
+
+await sql.end()
+
+// biome-ignore lint/suspicious/noConsole: <only in dev>
+console.log('Database seeded')
+// biome-ignore lint/suspicious/noConsole: <only in dev>
+console.log(env.DATABASE_URL)
